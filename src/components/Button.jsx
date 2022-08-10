1d7f6273eab21cf95665d6e975eb2283a0bcb43d
= <=>
2022-08-10 15:36:49 +0700

    home page 1

---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Button = ({bgColor, color, size, text, borderRadius}) => {
+  return (
+    <button
+    type="button"
+    style={{backgroundColor: bgColor, borderRadius, color}}
+    className={`text-${size} p-3 hover:drop-shadow-xl`}>{text}</button>
+  )
+}
+
+export default Button
